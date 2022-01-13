c95be1f1b12c484214b5b4894881e153fc513854
gustavozreis <gzreis@gmail.com>
2022-01-13 12:31:05 -0300

    Configurado importação de imagens do servidor usando Coil

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -31,6 +31,8 @@ class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<String>()
+    private val _photos = MutableLiveData<MarsPhoto>()
+    val photos: LiveData<MarsPhoto> = _photos
 
     // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
@@ -48,8 +50,8 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+                _photos.value = MarsApi.retrofitService.getPhotos()[0]
+                _status.value = "   First Mars image URL : ${_photos.value!!.imgSrcUrl}"
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
