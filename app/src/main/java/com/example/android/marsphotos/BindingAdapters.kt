c84c42955eb372a1aa7888874c4ebe894730b798
gustavozreis <gzreis@gmail.com>
2022-01-13 15:18:39 -0300

    Adicionado gerenciamento de erros no RecyclerView

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,12 +1,14 @@
 package com.example.android.marsphotos
 
 import android.provider.ContactsContract
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 class BindingAdapters {
@@ -26,6 +28,23 @@ class BindingAdapters {
     fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsPhoto>?) {
         val adapter = recyclerView.adapter as PhotoGridAdapter
         adapter.submitList(data)
+    }
+
+    @BindingAdapter("marsApiStatus")
+    fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+        when (status) {
+            MarsApiStatus.LOADING -> {
+                statusImageView.visibility = View.VISIBLE
+                statusImageView.setImageResource((R.drawable.loading_animation))
+            }
+            MarsApiStatus.ERROR -> {
+                statusImageView.visibility = View.VISIBLE
+                statusImageView.setImageResource((R.drawable.ic_connection_error))
+            }
+            MarsApiStatus.DONE -> {
+                statusImageView.visibility = View.GONE
+            }
+        }
     }
 
 }
